7c0e3da9f6913faae5c8e59e7df100f9586b9258
sodacan^v^ <reinwaldmarone.valeza.320401@gmail.com>
2026-06-20 02:21:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",   // relative paths — works for GitHub Pages
+  base: '/The-Dredge/',
 });
